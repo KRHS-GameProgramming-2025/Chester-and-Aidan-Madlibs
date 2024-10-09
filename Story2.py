979a9dcafb57c8a5237ebe799f62951fabd5b482
Aidan Ferreira <aidan.ferreira@kearsarge.org>
2024-10-09 09:17:57 -0400

    started sotry 2

--- Story2.py
+++ Story2.py
@@ -1,1 +1,13 @@
+from Getword import *
+
+def Story2(debug = False):
+    if debug: print("Story1 Function")
+    
+    print("\n")
+    weekday = getWord("Enter a day of the week: ", debug)
+   
+    out = "\n"
+    out += "One " + weekday + ", "
+    
+    return out
 
