6c8516e298292342b05dd0d25fb47c9ab45b80bf
Chester <lillian.holl@kearsarge.org>
2024-10-15 07:45:26 -0400

    Story 1 addtion

--- Story1.py
+++ Story1.py
@@ -4,9 +4,10 @@ def Story1(debug = False):
     if debug: print("Story1 Function")
     
     print("\n")
-    friendName1 = getWord("Enter a name: ", debug)
+    friendName1 = getWord("Enter a Name: ", debug)
     sport1 = getSport("Enter a Sport: ", debug)
     anmial1 = getAnmial("Enter a Anmial: ", debug)
+    verb1 = getWored("Enter a Verb: ", debug)
    
     out = "\n"
     out += "One day my friend, " + friendName1 + ", and I"
