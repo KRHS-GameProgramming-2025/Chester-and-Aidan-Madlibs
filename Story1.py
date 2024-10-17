8a3772eabd98bd69ad8022d7826fd147b7e6fce1
Chester <lillian.holl@kearsarge.org>
2024-10-17 08:27:46 -0400

    Story 1 = done, made new files

--- Story1.py
+++ Story1.py
@@ -7,7 +7,7 @@ def Story1(debug = False):
     friendName1 = getWord("Enter a Name: ", debug)
     sport1 = getSport("Enter a Sport: ", debug)
     anmial1 = getAnmial("Enter a Anmial: ", debug)
-    verb1 = getWord("Enter a Verb: ", debug)
+    verb1 = getWord("Enter a Verb ending in 'ed': ", debug)
     pronoun1 = getWord("Enter a Pronoun: ", debug)
     color1 = getWord("Enter a Color: ", debug)
     bakedGood1 = getWord("Enter a Baked Good: ", debug)
@@ -17,10 +17,11 @@ def Story1(debug = False):
     out += "One day my friend, " + friendName1 + ", and I"
     out += " were out playing " + sport1
     out += " and we saw someone walking a " + anmial1
-    out += " it came over to us and " + verb1 + " "
+    out += " it came over to us and " + verb1 + " us "
     out += friendName1 + " was scared of the " + anmial1 + " so " + pronoun1
     out += " went to the " + color1 + " house down the street"
     out += " I went after " + pronoun2 + " to be a friend, but when I got there " + pronoun1 + " was eating a " + bakedGood1 
-    out += " I asked where " + pronoun1 + " got the " + bakedGood1
+    out += " I asked where " + pronoun1 + " got the " + bakedGood1 + " "
+    out += pronoun1 + " explained that this was a good friend and the " + anmial1 + " was hers, after that we both started laughing, for " + pronoun1 +  " was scared for nothing" 
     
     return out
